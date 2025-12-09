cb772ab5fe46d63b572139d93dead24fd4793599
Alex Mazansky <11902347+amazansky@users.noreply.github.com>
2025-12-08 19:58:32 -0500

    bump package versions

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,14 @@
-import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
 }
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins:['remark-gfm'],
     rehypePlugins: [],
   },
 })
